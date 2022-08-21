f1364f57398515230f7cc7c9c7643648f15576db
younghoSNU <hois1998@snu.ac.kr>
2022-08-21 12:51:51 +0900

    20220821

--- 12100숫자게임2048_삼성SW.js
+++ 12100숫자게임2048_삼성SW.js
@@ -9,7 +9,7 @@ let input = fs.readFileSync('./test.txt').toString().trim().split('\n').map(str 
 let N = +input.shift()
 input = input.map(str => str.split(' ').map(chr => +chr))
 
-console.log(N, input)
+// console.log(N, JSON.stringify(input))
 
 /**
  * 
@@ -20,22 +20,26 @@ const findNextMat = (mat, cnt) => {
 
     if (cnt == 5) {
         let max = 0
-        for (let e of mat) {
-            if (e > max) max = e
+        for (let row of mat) {
+            for (let e of row) {
+                if (e > max) max = e
+            }
         }
+
         return max
     }
-    let max 
-    let loc //4가지 방향을 위해 사용하는 변수
-    let nMat = [null, null, null, null]
-    let fourDirectionResult = [null, null, null, null]
 
-    for (loc=0; loc<4; ++loc) {
-        
+    let max //fourdirection의 최대값들을 모은 상태에서 그것들 중 무엇이 젤 큰지 찾을 때 사용하는 변수
+    let nMat = [null, null, null, null]
+    let fourDirectionResult = [0, 0, 0, 0]
+
+    for (let loc=0; loc<4; ++loc) { //loc은 4가지 방향의 움직임을 나타냈
+        // let isChanged = false //합쳐지는 것 유무로 true로 바꾸고 해당 방향으로 cnt를 올리는 식으로 진행 //근데 생각해보니까 합쳐지는 것이 없어도 다음 이동에서 합쳐질수도 있으니까
+        let nMat = [...Array(N)].map(_ => [...Array(N)].fill(0))
+
         if (loc==0) 
         {
             let pArr
-            nMat[loc] = [...Array(N)].map(_ => [...Array(N)].fill(null))
             let pLen
             let nMatCnt 
 
@@ -45,21 +49,22 @@ const findNextMat = (mat, cnt) => {
                 for (let row=0; row<N; ++row) {
                     let e = mat[row][col]
                     if (e) {
-                        pArr.push(e)
+                        pArr.push(e)    //어차피 여기서 pArr엔 0이 안들어가
                     }
                 }
                 
                 pLen = pArr.length
                 nMatCnt = 0
 
-                for (let i=1; i<pLen; ++i) {
+                for (let i=0; i<pLen; ++i) {
                     let pE = pArr[i]
     
-                    if (pArr[i-1] == pE) {
+                    if (i < pLen-1 && pArr[i+1] == pE) {   //+를 붙인 이유는 null이면 +null => 0이고 0 != null이기 때문 //이것을 생각할 필요가 없었어 왜냐면 pArr엔 null이나 0이 안들어가기 때문에
+                        // isChanged = true
                         ++i
-                        nMat[loc][nMatCnt][col] = pE*2
+                        nMat[nMatCnt][col] = pE*2
                     } else {
-                        nMat[loc][nMatCnt][col] = pE
+                        nMat[nMatCnt][col] = pE
 
                     }
                     ++nMatCnt
@@ -67,10 +72,9 @@ const findNextMat = (mat, cnt) => {
             }
             
         }
-        else if (loc == 2)
+        else if (loc == 2)  //아래로 이동
         {
             let pArr
-            nMat[loc] = [...Array(N)].map(_ => [...Array(N)].fill(null))
             let pLen
             let nMatCnt 
 
@@ -87,24 +91,24 @@ const findNextMat = (mat, cnt) => {
                 pLen = pArr.length
                 nMatCnt = N-1
 
-                for (let i=pLen-2; i>=0; --i) {
+                for (let i=pLen-1; i>=0; --i) {
                     let pE = pArr[i]
     
-                    if (pArr[i+1] == pE) {
+                    if (i > 0 && pArr[i-1] == pE) {
+                        // isChanged = true
                         --i
-                        nMat[loc][nMatCnt][col] = pE*2
+                        nMat[nMatCnt][col] = pE*2
                     } else {
-                        nMat[loc][nMatCnt][col] = pE
+                        nMat[nMatCnt][col] = pE
 
                     }
                     --nMatCnt
                 }
             }
         }
-        else if (loc == 1)
+        else if (loc == 1)  //오른쪽
         {
             let pArr 
-            nMat[loc] = [...Array(N)].map(_ => [...Array(N)].fill(null))
             let pLen
             let nMatCnt 
 
@@ -121,14 +125,15 @@ const findNextMat = (mat, cnt) => {
                 pLen = pArr.length
                 nMatCnt = N-1
 
-                for (let i=pLen-2; i>=0; --i) {
+                for (let i=pLen-1; i>=0; --i) {
                     let pE = pArr[i]
     
-                    if (pArr[i+1] == pE) {
+                    if (i > 0 && pArr[i-1] == pE) {
+                        // isChanged = true
                         --i
-                        nMat[loc][row][nMatCnt] = pE*2
+                        nMat[row][nMatCnt] = pE*2
                     } else {
-                        nMat[loc][row][nMatCnt] = pE
+                        nMat[row][nMatCnt] = pE
 
                     }
                     --nMatCnt
@@ -138,7 +143,6 @@ const findNextMat = (mat, cnt) => {
         else 
         {
             let pArr
-            nMat[loc] = [...Array(N)].map(_ => [...Array(N)].fill(null))
             let pLen
             let nMatCnt 
 
@@ -155,14 +159,15 @@ const findNextMat = (mat, cnt) => {
                 pLen = pArr.length
                 nMatCnt = 0
 
-                for (let i=1; i<pLen; ++i) {
+                for (let i=0; i<pLen; ++i) {
                     let pE = pArr[i]
     
-                    if (pArr[i-1] == pE) {
+                    if (i < pLen-1 && pArr[i+1] == pE) {
+                        // isChanged = true
                         ++i
-                        nMat[loc][row][nMatCnt] = pE*2
+                        nMat[row][nMatCnt] = pE*2
                     } else {
-                        nMat[loc][row][nMatCnt] = pE
+                        nMat[row][nMatCnt] = pE
 
                     }
                     ++nMatCnt
@@ -170,13 +175,14 @@ const findNextMat = (mat, cnt) => {
             }
         }
 
-        if (JSON.stringify(nMat[loc]) != JSON.stringify(mat)) {
-            fourDirectionResult[loc] = findNextMat(nMat[loc], cnt+1)
+        if (JSON.stringify(nMat) != JSON.stringify(mat)) {
+            fourDirectionResult[loc] = findNextMat(nMat, cnt+1)
         }
     }
 
+    max = 0
+    //각 방향에서 제일 큰 수를 뽑아오는 것인데 뽑아오면 그 상위 fourDirectionResult에 전달한다
     for (let e of fourDirectionResult) {
-        max = 0
         if (e > max) max = e
     }
 
